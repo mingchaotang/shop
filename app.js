1ccc0860c977b55e0301f84f9f78eab55fd7b933
mingchaotang <mingchao.tang@gmail.com>
2019-08-09 17:43:17 -0400

    parsing incoming request

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-  console.log('This will always run');
-  next();
-});
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/add-product', (req, res, next) => {
-  res.send('<h1>The "Add Product" Page</h1>');
+  res.send(
+      '<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+});
+
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
