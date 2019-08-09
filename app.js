dc71db22b2c56787b04d030c91d523c977648eec
mingchaotang <mingchao.tang@gmail.com>
2019-08-09 17:15:29 -0400

    how middleware works; handling different route

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the Middleware");
+app.use('/', (req, res, next) => {
+  console.log('This will always run');
   next();
 });
 
-app.use((req, res, next) => {
-  console.log("In another Middleware");
+app.use('/add-product', (req, res, next) => {
+  res.send('<h1>The "Add Product" Page</h1>');
 });
 
-const server = http.createServer(app);
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
 
-server.listen(3000);
+app.listen(3000);
