1d8df375db83d878a474013b570cfe8fd7530323
mingchaotang <mingchao.tang@gmail.com>
2019-08-13 15:32:51 -0400

    serving html pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,13 @@
+const path = require('path');
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-  res.send(
-      '<form action="/product" method="post"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
