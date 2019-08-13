60aeea789d90c3ab4551f2e184bb046b0c473043
mingchaotang <mingchao.tang@gmail.com>
2019-08-13 15:43:09 -0400

    returning a 404 page

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -10,8 +11,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page Not Found!</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
